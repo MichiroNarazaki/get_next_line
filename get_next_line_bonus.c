271fa0497d12b839737bc7af6553995f4ab88081
mi0706chi <mi0706chi@gmail.com>
2020-09-07 00:55:28 +0900

    remove free(line) and fix args and malloc

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -52,7 +52,6 @@ static int	output(char **s, char **line, int ret, int fd)
 		return (ERROR);
 	else if (ret == 0 && s[fd] == NULL)
 	{
-		free(*line);
 		if (!((*line) = ft_strdup("\0")))
 			return (ERROR);
 		return (EOF_REACHED);
@@ -61,10 +60,9 @@ static int	output(char **s, char **line, int ret, int fd)
 		return (make_line(&s[fd], line));
 }
 
-static int	get_next_line_main(char **s, char **line, int fd)
+static int	get_next_line_main(char **s, char **line, int fd, char *buff)
 {
 	char	*tmp;
-	char	buff[BUFFER_SIZE + 1];
 	ssize_t	ret;
 
 	while ((ret = read(fd, buff, BUFFER_SIZE)) > 0)
@@ -93,8 +91,11 @@ static int	get_next_line_main(char **s, char **line, int fd)
 int			get_next_line(const int fd, char **line)
 {
 	static char	*s[FOPEN_MAX];
+	char		*buff;
 
 	if (fd < 0 || FOPEN_MAX <= fd || BUFFER_SIZE <= 0)
 		return (ERROR);
-	return (get_next_line_main(s, line, fd));
+	if (!(buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (ERROR);
+	return (get_next_line_main(s, line, fd, buff));
 }
